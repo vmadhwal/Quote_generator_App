ff5fe6084f1ad044320c414e4e61b0c89bfc7520
vmadhwal <vaibhavmadhwal123@gmail.com>
2023-09-21 17:18:06 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const api_url='https://api.quotable.io/random';
+const quote=document.getElementById("quote");
+const author = document.getElementById("author");
+async function getquote(url){
+    const response= await fetch(url);
+    var data= await response.json();
+    console.log(data);
+    quote.innerHTML=data.content;
+    author.innerHTML=data.author;
+}
+getquote(api_url);
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=Hello%20world")
+}
